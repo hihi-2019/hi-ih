2601cc2a1e7bbef80d9e905b01b977822f2b4e2f
Student <student@eda-bc-06.localdomain>
2019-11-15 10:50:06 +1300

    user component

--- client/api/poke.js
+++ client/api/poke.js
@@ -3,12 +3,13 @@ import request from "superagent"
 
 export function getUsers(){
    return request.get('/api/v1/')
+   .then(res => res.body)
     
 }
 
 export function getAllPokemon(){
-    return request.get('`http://pokeapi.co/api/v2/pokemon/?limit=200')
-    .then(res => res.body.results)
+    return request.get('http://pokeapi.co/api/v2/pokemon/?limit=200')
+    // .then(res => res.body)
 }
 
 
