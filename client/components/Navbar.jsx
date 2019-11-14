af0d7949984487e9a232a999067bb36edffe2790
Student <student@eda-bc-06.localdomain>
2019-11-14 17:47:22 +1300

    add navbar

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+function Navbar(){
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </nav>
+    )
+}
+
+export default Navbar
